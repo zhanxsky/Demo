9f3514a93cbccd08e66065740985447860c33139
zhanxsky <zhan1009@hotmail.com>
2015-08-28 10:53:45 +0800

    add SlideView

--- SlideView/src/main/java/com/zhan/demo/MainActivity.java
+++ SlideView/src/main/java/com/zhan/demo/MainActivity.java
@@ -1,37 +1,162 @@
 package com.zhan.demo;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener, View.OnClickListener,
+        SlideView.OnSlideListener {
+
+    private static final String TAG = "MainActivity";
+
+    private ListViewCompat mListView;
+
+    private List<MessageItem> mMessageItems = new ArrayList<MessageItem>();
+
+    private SlideView mLastSlideViewWithStatusOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initView() {
+        mListView = (ListViewCompat) findViewById(R.id.list);
+
+        for (int i = 0; i < 20; i++) {
+            MessageItem item = new MessageItem();
+            if (i % 3 == 0) {
+                item.iconRes = R.mipmap.default_qq_avatar;
+                item.title = "腾讯新闻";
+                item.msg = "青岛爆炸满月：大量鱼虾死亡";
+                item.time = "晚上18:18";
+            } else {
+                item.iconRes = R.mipmap.wechat_icon;
+                item.title = "微信团队";
+                item.msg = "欢迎你使用微信";
+                item.time = "12月18日";
+            }
+            mMessageItems.add(item);
+        }
+        mListView.setAdapter(new SlideAdapter());
+        mListView.setOnItemClickListener(this);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private class SlideAdapter extends BaseAdapter {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        private LayoutInflater mInflater;
+
+        SlideAdapter() {
+            super();
+            mInflater = getLayoutInflater();
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public int getCount() {
+            return mMessageItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mMessageItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            SlideView slideView = (SlideView) convertView;
+            if (slideView == null) {
+                View itemView = mInflater.inflate(R.layout.list_item, null);
+                slideView = new SlideView(MainActivity.this);
+                slideView.setContentView(itemView);
+
+                holder = new ViewHolder(slideView);
+                slideView.setOnSlideListener(MainActivity.this);
+                slideView.setTag(holder);
+            } else {
+                holder = (ViewHolder) slideView.getTag();
+            }
+            MessageItem item = mMessageItems.get(position);
+            item.slideView = slideView;
+            item.slideView.shrink();
+
+            holder.icon.setImageResource(item.iconRes);
+            holder.title.setText(item.title);
+            holder.msg.setText(item.msg);
+            holder.time.setText(item.time);
+            holder.deleteHolder.setOnClickListener(MainActivity.this);
+
+            return slideView;
+        }
+
+    }
+
+    public class MessageItem {
+        public int iconRes;
+        public String title;
+        public String msg;
+        public String time;
+        public SlideView slideView;
+    }
+
+    private static class ViewHolder {
+        public ImageView icon;
+        public TextView title;
+        public TextView msg;
+        public TextView time;
+        public ViewGroup deleteHolder;
+
+        ViewHolder(View view) {
+            icon = (ImageView) view.findViewById(R.id.icon);
+            title = (TextView) view.findViewById(R.id.title);
+            msg = (TextView) view.findViewById(R.id.msg);
+            time = (TextView) view.findViewById(R.id.time);
+            deleteHolder = (ViewGroup)view.findViewById(R.id.holder);
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+                            long id) {
+        Log.e(TAG, "onItemClick position=" + position);
+    }
+
+    @Override
+    public void onSlide(View view, int status) {
+        if (mLastSlideViewWithStatusOn != null && mLastSlideViewWithStatusOn != view) {
+            mLastSlideViewWithStatusOn.shrink();
+        }
+
+        if (status == SLIDE_STATUS_ON) {
+            mLastSlideViewWithStatusOn = (SlideView) view;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.holder) {
+            Log.e(TAG, "onClick v=" + v);
+        }
     }
 }
+
